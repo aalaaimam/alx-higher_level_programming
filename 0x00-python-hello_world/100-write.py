5e3c046a7812fdb2da2e5846c448c04632f9c025
aalaaimam <aalaaimam@gmail.com>
2024-04-03 05:05:28 +0000

    0x00-python-hello_world

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,21 +1,4 @@
-#include "lists.h"
-
-/**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: pointer to the head of the list
- * Return: 0 if there is no cycle, 1 if there is a cycle
- */
-int check_cycle(listint_t *list)
-{
-    listint_t *slow = list, *fast = list;
-
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-            return (1);
-    }
-
-    return (0);
-}
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
