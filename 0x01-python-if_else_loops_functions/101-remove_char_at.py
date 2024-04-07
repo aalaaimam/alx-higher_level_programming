812e31bc1a5c49864130cc269a88e9a54b76cfa3
aalaaimam <aalaaimam@gmail.com>
2024-04-07 04:38:41 +0000

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,5 @@
-har_at(str, n):
+#!/usr/bin/python3
+def remove_char_at(str, n):
     if n < 0:
-        return str
-    else:
-        str = str[0:n] + str[n+1:]
-    return str
+        return (str)
+    return (str[:n] + str[n+1:])
