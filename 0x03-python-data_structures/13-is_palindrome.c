c696f4d97f3cecc95d4afa6fe206050aea2a23e7
aalaaimam <aalaaimam@gmail.com>
2024-04-08 23:46:49 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,37 @@
 #include "lists.h"
-#include <stddef.h>
-/**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the head of the list
  *
- * Return: 1 if it is, 0 if not
+ * Return: 1 if the list is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+    listint_t *slow = *head, *fast = *head, *prev = NULL, *temp;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
-	while (1)
-	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
-	}
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
 
-	reverse_listint(&dup);
+    if (fast)
+        slow = slow->next;
 
-	while (dup && temp)
-	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
-			return (0);
-	}
+    while (slow && prev)
+    {
+        if (slow->n != prev->n)
+            return (0);
+        slow = slow->next;
+        prev = prev->next;
+    }
 
-	if (!dup)
-		return (1);
-
-	return (0);
+    return (1);
 }
