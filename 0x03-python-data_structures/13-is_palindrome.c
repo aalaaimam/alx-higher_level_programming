4b7239cad0bcc5635028ebcc896105e59ffa941c
aalaaimam <125838827+aalaaimam@users.noreply.github.com>
2024-04-09 22:48:52 +0200

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,59 +1,111 @@
 #include "lists.h"
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * reverse_list - Reverse a linked list
- * @head: pointer to head of list
+ * reverse_listint - reverses a linked list
+ * @head: pointer to pointer of first node of listint_t list
  * Return: pointer to the new head of the reversed list
  */
-listint_t *reverse_list(listint_t **head)
+listint_t *reverse_listint(listint_t **head)
 {
     listint_t *prev = NULL;
-    listint_t *current = *head;
-    listint_t *next;
+    listint_t *next = NULL;
 
-    while (current != NULL)
+    while (*head != NULL)
     {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = next;
     }
 
-    *head = prev;
-    return *head;
+    return prev;
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome
+ * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to pointer of first node of listint_t list
- * Return: 1 if the linked list is a palindrome, 0 otherwise
+ * Return: 1 if the list is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
     listint_t *slow = *head;
     listint_t *fast = *head;
+    listint_t *second_half = NULL;
+    listint_t *prev_slow = *head;
+    listint_t *mid_node = NULL;
+    int result = 1; // Assume it's a palindrome by default
 
-    while (fast->next != NULL && fast->next->next != NULL)
+    if (*head != NULL && (*head)->next != NULL)
     {
-        slow = slow->next;
-        fast = fast->next->next;
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            prev_slow = slow;
+            slow = slow->next;
+        }
+
+        // If the linked list has odd number of elements, ignore the middle element
+        if (fast != NULL)
+        {
+            mid_node = slow;
+            slow = slow->next;
+        }
+
+        // Reverse the second half and compare with the first half
+        second_half = slow;
+        prev_slow->next = NULL; // Mark the end of the first half
+        second_half = reverse_listint(&second_half); // Reverse the second half
+
+        // Compare the first half and the reversed second half
+        result = compare_lists(*head, second_half);
+
+        // Restore the original list by reversing the reversed second half
+        second_half = reverse_listint(&second_half);
+
+        // If there was a mid node (odd length list), reconnect it
+        if (mid_node != NULL)
+        {
+            prev_slow->next = mid_node;
+            mid_node->next = second_half;
+        }
+        else
+        {
+            prev_slow->next = second_half;
+        }
     }
 
-    slow->next = reverse_list(&slow->next);
-    slow = slow->next;
-    fast = *head;
+    return result;
+}
 
-    while (slow != NULL)
+/**
+ * compare_lists - compares two linked lists
+ * @head1: pointer to pointer of first node of listint_t list
+ * @head2: pointer to pointer of first node of listint_t list
+ * Return: 1 if the lists are identical, 0 otherwise
+ */
+int compare_lists(listint_t *head1, listint_t *head2)
+{
+    listint_t *temp1 = head1;
+    listint_t *temp2 = head2;
+
+    while (temp1 && temp2)
     {
-        if (fast->n != slow->n)
-            return (0);
-        fast = fast->next;
-        slow = slow->next;
+        if (temp1->n == temp2->n)
+        {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
-    return (1);
+    // Both lists are identical if temp1 and temp2 are NULL
+    if (temp1 == NULL && temp2 == NULL)
+        return 1;
+
+    return 0;
 }
