79060aec1142def344940ff422899221cb3eb465
aalaaimam <aalaaimam@gmail.com>
2024-04-09 15:44:08 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,40 +1,45 @@
-#include <stdio.h>
 #include <Python.h>
 
-void print_python_list(PyObject *p) {
+void print_python_list(PyObject *p)
+{
     Py_ssize_t size, i;
     PyObject *item;
 
-    printf("[*] Python list info\n");
     size = PyList_Size(p);
+    printf("[*] Python list info\n");
     printf("[*] Size of the Python List = %ld\n", size);
-
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-    for (i = 0; i < size; i++) {
+
+    for (i = 0; i < size; ++i)
+    {
         item = PyList_GetItem(p, i);
         printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
 
-void print_python_bytes(PyObject *p) {
+void print_python_bytes(PyObject *p)
+{
     Py_ssize_t size, i;
-    char *str;
+    unsigned char *string;
 
     printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p)) {
+
+    if (!PyBytes_Check(p))
+    {
         printf("  [ERROR] Invalid Bytes Object\n");
         return;
     }
 
     size = PyBytes_Size(p);
+    string = (unsigned char *)PyBytes_AsString(p);
+
     printf("  size: %ld\n", size);
-    
-    printf("  trying string: %s\n", PyUnicode_AsUTF8AndSize(p, &size));
-    
-    printf("  first %ld bytes:", (size > 10) ? 10 : size);
-    str = PyBytes_AsString(p);
-    for (i = 0; i < ((size > 10) ? 10 : size); i++) {
-        printf(" %02x", str[i] & 0xFF);
+    printf("  trying string: %s\n", (char *)string);
+
+    printf("  first %ld bytes: ", (size > 10) ? 10 : size);
+    for (i = 0; i < ((size > 10) ? 10 : size); ++i)
+    {
+        printf("%02x ", string[i]);
     }
     printf("\n");
 }
