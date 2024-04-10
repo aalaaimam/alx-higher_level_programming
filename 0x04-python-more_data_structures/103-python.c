fe78b079d3fb54db819a45279c301fe04e21535a
aalaaimam <125838827+aalaaimam@users.noreply.github.com>
2024-04-10 02:37:36 +0200

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -23,7 +23,7 @@ void print_python_list(PyObject *p)
             printf("Element %ld: <failed to retrieve>\n", i);
             continue;
         }
-        printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
+        printf("Element %ld: %s\n", i, element->ob_type->tp_name); // Changed Py_TYPE to element->ob_type
         Py_DECREF(element);
     }
 }
