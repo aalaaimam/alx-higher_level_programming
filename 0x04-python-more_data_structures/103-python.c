92c141145239b9ad50ac19c9026fed49015f8982
aalaaimam <aalaaimam@gmail.com>
2024-04-09 18:30:12 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -17,14 +17,26 @@ void print_python_list(PyObject *p)
 
     for (i = 0; i < size; i++)
     {
-        element = PySequence_GetItem(p, i);
-        if (!element)
+        element = PyList_GetItem(p, i);
+        printf("Element %ld: ", i);
+        if (PyBytes_Check(element))
         {
-            printf("Element %ld: <failed to retrieve>\n", i);
-            continue;
+            printf("bytes\n");
+            printf("[.] bytes object info\n");
+            printf("  size: %ld\n", PyBytes_Size(element));
+            printf("  trying string: %s\n", PyBytes_AsString(element));
+            printf("  first 6 bytes: ");
+            unsigned char *str = (unsigned char *)PyBytes_AsString(element);
+            for (int j = 0; j < 6; j++)
+            {
+                printf("%02x ", str[j]);
+            }
+            printf("\n");
         }
-        printf("Element %ld: %s\n", i, element->ob_type->tp_name);
-        Py_DECREF(element);
+        else
+        {
+            printf("%s\n", Py_TYPE(element)->tp_name);
+        }
     }
 }
 
@@ -34,7 +46,7 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-    Py_ssize_t i, size;
+    Py_ssize_t size;
     unsigned char *str;
 
     printf("[.] bytes object info\n");
@@ -49,10 +61,23 @@ void print_python_bytes(PyObject *p)
     str = (unsigned char *)PyBytes_AsString(p);
 
     printf("  size: %ld\n", size);
-    printf("  trying string: ");
-    for (i = 0; i < size && i < 10; i++)
+    printf("  trying string: %s\n", str);
+    printf("  first 6 bytes: ");
+    for (Py_ssize_t i = 0; i < size && i < 6; i++)
     {
         printf("%02x ", str[i]);
     }
     printf("\n");
+}
+
+int main() {
+    PyObject *l, *b;
+    Py_Initialize();
+    l = PyList_New(0);
+    b = PyBytes_FromString("What does the 'b' character do in front of a string literal?");
+    PyList_Append(l, b);
+    print_python_bytes(b);
+    print_python_list(l);
+    Py_Finalize();
+    return 0;
 }
