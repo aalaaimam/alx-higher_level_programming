a6054a02963bfa8efc4ca0c0ebdd9ce4e95ece9f
aalaaimam <aalaaimam@gmail.com>
2024-04-17 02:37:49 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/12-main.py
+++ 0x0B-python-input_output/12-main.py
@@ -8,13 +8,9 @@ def print_triangle(triangle):
     """
     Print the triangle
     """
-    if triangle is None:
-        print("Triangle is None")
-        return
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
-
