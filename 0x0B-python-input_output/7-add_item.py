0a0b959b3aac5c784b2340f484c358d5d71c1652
aalaaimam <aalaaimam@gmail.com>
2024-04-17 02:15:01 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """Save argument to a file."""
 import sys
+import json
+
+def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file, using a JSON representation."""
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
+def load_from_json_file(filename):
+    """Creates an Object from a "JSON file"."""
+    with open(filename, 'r') as file:
+        return json.load(file)
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
+
