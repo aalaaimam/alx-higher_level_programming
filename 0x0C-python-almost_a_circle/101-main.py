54b9e6dca7d067b6c84797b0a88e90caa700a8cc
aalaaimam <aalaaimam@gmail.com>
2024-04-20 09:09:36 -0700

    0x0C-python-almost_a_circles

--- 0x0C-python-almost_a_circle/101-main.py
+++ 0x0C-python-almost_a_circle/101-main.py
@@ -4,8 +4,13 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-if __name__ == "__main__":
+class Base:
+    @classmethod
+    def draw(cls, list_rectangles, list_squares):
+        # Your implementation of the draw method
+        pass
 
+if __name__ == "__main__":
     list_rectangles = [Rectangle(100, 40), Rectangle(90, 110, 30, 10), Rectangle(20, 25, 110, 80)]
     list_squares = [Square(35), Square(15, 70, 50), Square(80, 30, 70)]
 
