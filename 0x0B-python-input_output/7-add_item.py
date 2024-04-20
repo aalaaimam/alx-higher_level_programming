eb6ebf991d5e97cb23253176455fe91f13eff564
aalaaimam <aalaaimam@gmail.com>
2024-04-20 09:23:06 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""Save argument to a file."""
+"""Save arguments to and load from a JSON file."""
 import sys
 import json
 
+
 def save_to_json_file(my_obj, filename):
-    """Writes an Object to a text file, using a JSON representation."""
+    """Write an Object to a file using JSON representation."""
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
 
+
 def load_from_json_file(filename):
-    """Creates an Object from a "JSON file"."""
+    """Create an Object from a JSON file."""
     with open(filename, 'r') as file:
         return json.load(file)
+
 
 if __name__ == "__main__":
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+    
+    # Extend items list with command-line arguments
     items.extend(sys.argv[1:])
+    
+    # Save the updated items list back to "add_item.json"
     save_to_json_file(items, "add_item.json")
-
