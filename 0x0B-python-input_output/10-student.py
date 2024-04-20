1afdaa5622003495b71bc23fb729617dc8f33d55
aalaaimam <aalaaimam@gmail.com>
2024-04-20 09:33:04 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,15 +6,13 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize student props.
-        """
+        """Initialize student props."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        """Return JSON representation of the Student instance."""
+        if attrs is not None and isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
