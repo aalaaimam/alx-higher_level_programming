0e5cc3cdcc491808327d9f210e6a7de7b170126f
aalaaimam <aalaaimam@gmail.com>
2024-04-20 10:07:27 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,6 @@ class Student:
 
     def to_json(self, attrs=None):
         """Return JSON representation of the Student instance."""
-        if attrs is not None and isinstance(attrs, list) and \
-           all(isinstance(attr, str) for attr in attrs):
+        if attrs is not None and isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
             return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
