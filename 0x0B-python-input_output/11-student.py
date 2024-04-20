d127870b8df391c3d94529dbfd108abe3057e05b
aalaaimam <aalaaimam@gmail.com>
2024-04-20 10:01:43 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,5 +26,4 @@ class Student:
 
 # Pycodestyle validation
 if __name__ == "__main__":
-    # This ensures that the code is only executed when the script is run directly
-    pass
+    pass  # This ensures that the code is only executed when the script is run directly
