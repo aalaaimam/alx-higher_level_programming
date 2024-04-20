d377fdde8b26ba017693e18003cd5dfc104584dc
aalaaimam <aalaaimam@gmail.com>
2024-04-20 09:42:09 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,18 +6,23 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize student props
-        """
+        """Initialize student props."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        """Converts object attributes to a JSON-compatible dictionary."""
+        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
+        """Reloads object attributes from a JSON dictionary."""
         for k, v in json.items():
             setattr(self, k, v)
+
+
+# Pycodestyle validation
+if __name__ == "__main__":
+    pass  # This ensures that the code is only executed when the script is run directly
